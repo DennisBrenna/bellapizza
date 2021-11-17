1aa28f56dea3a397b1b97386120a53c551647ae9
gardemanuelsen <74247742+gardemanuelsen@users.noreply.github.com>
2021-11-17 22:50:16 +0100

    Final push off the day (checkout work)

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,15 +1,69 @@
 import React from 'react'
 import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import { backdropClasses, Paper } from "@mui/material";
 import Header from '../components/Header'
 
 export default function Checkout() {
+
+    const Item = styled(Paper)(({ theme }) => ({
+        ...theme.typography.body1,
+        padding: theme.spacing(1),
+        textAlign: 'left',
+        alignItems: "flex-end",
+        color: "white",
+        backgroundColor: "yellow",
+        boxShadow: "white",
+      }));
+
+      const Title = styled(Paper)(({ theme }) => ({
+        ...theme.typography.body1,
+        padding: theme.spacing(1),
+        textAlign: 'left',
+        alignItems: "flex-end",
+        color: "white",
+        backgroundColor: "red",
+        boxShadow: "white",
+      }));
+
+
+
     return (
         <>
             <Header/>
-            <Grid class="checkoutCard">
-                <h1>Items</h1>
-                
+            <div>
+            <Grid container>
+                <Grid>
+                <Title md={6}><h1>Items</h1></Title>
+                </Grid>
+               <Grid>
+               <Title md={6}><h1>Price</h1></Title>
+               </Grid>
             </Grid>
+
+
+            <Grid container class="One">
+                <Grid>
+                <Title xs={6}> hei</Title>
+                </Grid>
+                <Grid>
+                <Title xs={2}> hei</Title>
+                </Grid>
+                <Grid>
+                <Item md={2}> hei</Item>
+                </Grid>
+                <Grid>
+                <Item md={2}> hei</Item>
+                </Grid>
+                <Grid>
+                <Item md={2}> hei</Item>
+                </Grid>
+           
+           
+            </Grid>
+            </div>
+
+     
 
             
             
