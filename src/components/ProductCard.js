a4d896750cdcc9f1911c421e1f2b06be95f85d4e
DennisBrenna <dennis-1@live.se>
2021-11-19 11:57:05 +0100

    center

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,7 +10,18 @@ import Fab from '@mui/material/Fab';
 import pasta from '../images/warmpizza.jpeg';
 import AddIcon from '@mui/icons-material/Add';
 import Divider from '@mui/material/Divider';
+import { styled } from '@mui/material/styles';
+import { backdropClasses, Paper } from "@mui/material";
 
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body1,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  alignItems: "flex-end",
+  color: "white",
+  backgroundColor: "black",
+  boxShadow: "white",
+}));
 
 export default function ProductCard({ title, description, dish }) {
   return (
@@ -21,6 +32,8 @@ export default function ProductCard({ title, description, dish }) {
           style={{ height: "50%"}}
           component="img"
           image={ dish === 'pizza' ? pizza : pasta } alt={dish} width="82px" height="100px"/>
+
+        <Item>
         <CardContent style={{height: "50%", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
           <Typography gutterBottom variant="h7" component="div" textAlign="center">
             { title }
@@ -31,6 +44,7 @@ export default function ProductCard({ title, description, dish }) {
             { description }
           </Typography>
         </CardContent>
+        </Item>
       </CardActionArea>
       <CardActions style={{justifyContent: "center", alignContent: "center", marginTop: "4px", height: "50px"}}>
       <Fab size="small" aria-label="add" href="http://www.nhl.com">
