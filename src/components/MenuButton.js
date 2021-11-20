ea1d59c323bcf69726589cebb2a36fd5e691c1e1
Carlzon50 <72607945+Carlzon50@users.noreply.github.com>
2021-11-20 16:01:17 +0100

    card button and style

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -3,18 +3,15 @@ import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import React, { useState, useEffect } from 'react';
-
+import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom'
 import Logo from '../images/logo.png'
-
+import { backdropClasses, Paper } from "@mui/material";
 import Box from '@mui/material/Box';
 import { Grid } from '@mui/material';
 
 
 function MenuButton({products, filteredProducts}) {
-
-
-
 
     return (
 
@@ -26,8 +23,8 @@ function MenuButton({products, filteredProducts}) {
        direction="column"
        alignItems="center"
        justifyContent="center"
-       marginTop="15px"
-       marginBottom="10px"
+    
+
       >
         
       
